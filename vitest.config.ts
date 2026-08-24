997e628cc08132dcce37defcd8cc2b02ff2905b1
Jason Sugiharto <jasonliesugiharto@gmail.com>
2026-08-24 16:11:19 -0700

    added in vitest as well as convex test for unit testing of convex functions

---
+++ vitest.config.ts
@@ -0,0 +1,26 @@
+// vitest.config.ts
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    projects: [
+      {
+        extends: true,
+        test: {
+          name: "convex",
+          include: ["convex/**/*.test.{ts,js}"],
+          environment: "edge-runtime",
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: "frontend",
+          include: ["**/*.test.{ts,tsx,js,jsx}"],
+          exclude: ["convex/**"],
+          environment: "jsdom",
+        },
+      },
+    ],
+  },
+});
